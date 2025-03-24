b1bf4e1b1ac1349b44682356cd213deb49212a86
catalogo <lojaintegrada9@gmail.com>
2025-03-24 15:28:53 -0300

    Update login.py

--- login.py
+++ login.py
@@ -1,13 +1,16 @@
-import streamlit as st
-
 # Tela de login
-st.title("Tela de Login")
-usuario = st.text_input("Usuário")
-senha = st.text_input("Senha", type="password")
+if 'page' not in st.session_state:
+    st.session_state.page = "login"
 
-if st.button("Entrar"):
-    if usuario == "admin" and senha == "admin":  # Exemplo de validação simples
-        st.success("Login realizado com sucesso!")
-        # Realize o redirecionamento ou lógica de sucesso aqui
-    else:
-        st.error("Usuário ou senha incorretos!")
+# Se a página for login
+if st.session_state.page == "login":
+    st.title("Tela de Login")
+    usuario = st.text_input("Usuário")
+    senha = st.text_input("Senha", type="password")
+
+    if st.button("Entrar"):
+        if usuario == "admin" and senha == "admin":  # Exemplo de validação simples
+            st.session_state.page = "conta_virtual"  # Após login, redireciona para a tela de conta virtual
+            st.experimental_rerun()  # Atualiza a página para mostrar a tela de conta virtual
+        else:
+            st.error("Usuário ou senha incorretos!")
